8c05a3eb2ff85ab18f3dbc5315d2bb0a3b6300eb
André De Bem <andrebemferraz@gmail.com>
2021-02-25 20:08:09 -0300

    Request III

--- FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
@@ -1,5 +1,5 @@
-let a = 7;
-let b = 3;
+let a = 17;
+let b = 10;
 
 let adicao = a + b;
 let subtracao = a - b;
@@ -20,3 +20,19 @@ if (a > b) {
 } else {
   console.log("São iguais")
 }
+
+c = 30;
+
+let maior = a;
+
+if (b > a) {
+  maior = b
+}
+
+if (c > maior) {
+  maior = c
+}
+
+// Pesquisei e encontrei uma fonte de pesquisa para a construção deste código no site: https://www.javascriptprogressivo.net/2018/08/Script-Recebe-Tres-Numeros-Mostra-Maior.html#:~:text=Depois%20um%20bot%C3%A3o%2C%20que%20chama,n%C3%BAmeros%20que%20o%20usu%C3%A1rio%20digitou.
+
+console.log(`Maior de três variáveis: ${maior}`)
