2a9e0c28c09e487947b693f90a9b54d1b90996a3
André De Bem <andrebemferraz@gmail.com>
2021-02-25 19:33:30 -0300

    Request I

---
+++ FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 7;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let resto = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(resto);
