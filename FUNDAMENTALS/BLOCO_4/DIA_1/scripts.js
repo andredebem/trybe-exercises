d1bb8409c3049d310fa6e82df462ad3407ad7ffc
André De Bem <andrebemferraz@gmail.com>
2021-02-25 21:35:25 -0300

    Request IV

--- FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
@@ -36,3 +36,13 @@ if (c > maior) {
 // Pesquisei e encontrei uma fonte de pesquisa para a construção deste código no site: https://www.javascriptprogressivo.net/2018/08/Script-Recebe-Tres-Numeros-Mostra-Maior.html#:~:text=Depois%20um%20bot%C3%A3o%2C%20que%20chama,n%C3%BAmeros%20que%20o%20usu%C3%A1rio%20digitou.
 
 console.log(`Maior de três variáveis: ${maior}`)
+
+let d = 584;
+
+if (d > 0) {
+  console.log("positive")
+} else if (d < 0) {
+  console.log("negative")
+} else {
+  console.log("zero")
+}
