47a324e264466d0e28b448551b67bc6728679c8c
André De Bem <andrebemferraz@gmail.com>
2021-02-26 15:55:03 -0300

    Request IX

--- FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_1/scripts.js
@@ -117,9 +117,15 @@ switch(true) {
 
 let num1 = 717;
 let num2 = 202;
-let num3 = 1009;
+let num3 = 1008;
 
 if ((num1 % 2 == 0) || (num2 % 2 == 0) || (num3 % 2 == 0)) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+if ((num1 % 2 != 0) || (num2 % 2 != 0) || (num3 % 2 != 0)) {
   console.log(true)
 } else {
   console.log(false)
