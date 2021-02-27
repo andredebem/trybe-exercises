19da4770d93d03dec9c102f809969e35bf19c5b8
André De Bem <andrebemferraz@gmail.com>
2021-02-27 20:41:08 -0300

    Request II

--- FUNDAMENTALS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_2/scripts.js
@@ -3,3 +3,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (index = 0; index < numbers.length; index++) {
   console.log(numbers[index])
 }
+
+let sum = 0;
+
+for (index = 0; index < numbers.length; index++) {
+  sum = sum + numbers[index]
+  }
+  console.log(`SOMA: ${sum}`)
