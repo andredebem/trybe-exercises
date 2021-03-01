d2543545ca538c63b44e1bb6cbb6916d0d27cffe
André De Bem <andrebemferraz@gmail.com>
2021-03-01 16:28:30 -0300

    Request VI

--- FUNDAMENTALS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_2/scripts.js
@@ -1,8 +1,12 @@
+// Request I ---------------------------------------------------------
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (index = 0; index < numbers.length; index++) {
   console.log(numbers[index])
 }
+
+// Request II ---------------------------------------------------------
 
 let sum = 0;
 
@@ -10,6 +14,8 @@ for (index = 0; index < numbers.length; index++) {
   sum = sum + numbers[index]
   }
 console.log(`SUM: ${sum}`)
+
+// Request III ---------------------------------------------------------
 
 let arithmeticAverage = 0;
 
@@ -21,11 +27,15 @@ arithmeticAverage = sum / numbers.length
 
 console.log(`ARITHMETIC AVERAGE: ${arithmeticAverage}`)
 
+// Request IV ---------------------------------------------------------
+
 if (arithmeticAverage > 20) {
   console.log("GREATER THAN 20")
 } else {
   console.log("LESS OR EQUAL TO 20")
 }
+
+// Request V ---------------------------------------------------------
 
 let greather = numbers[0];
 
@@ -36,3 +46,19 @@ for (index = 1; index < numbers.length; index++) {
 }
 
 console.log(greather)
+
+// Request VI ---------------------------------------------------------
+
+let oddNumbers = [];
+
+for (index = 0; index < numbers.length; index++) {
+  if (numbers[index] % 2 != 0) {
+    oddNumbers.push(numbers[index])
+  }
+}
+
+if (oddNumbers.length != 0) {
+    console.log(`${oddNumbers.length} odd number(s) found: ${oddNumbers}`)
+} else {
+  console.log("no odd values found")
+}
