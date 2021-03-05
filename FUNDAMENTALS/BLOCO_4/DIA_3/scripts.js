7af0909d25cbbd0ba4a118792d2300ea6b4c0dce
André De Bem <andrebemferraz@gmail.com>
2021-03-05 19:28:01 -0300

    Request III

--- FUNDAMENTALS/BLOCO_4/DIA_3/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_3/scripts.js
@@ -14,3 +14,14 @@ for (i = 0; i < n; i += 1) {
   conca = conca + "*"
   console.log(conca)
 }
+
+// Request III --------------------------------
+
+let firstSpaces = " ".repeat(n);
+let newSpaces = "";
+
+for (i = 1; i <= n; i += 1) {
+  firstSpaces = firstSpaces.slice(0, -1)
+  newSpaces = firstSpaces + "*".repeat(i)
+  console.log(newSpaces)
+}
