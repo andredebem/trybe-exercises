345187e814230d4ef86ea72964d2bb7895ec072b
André De Bem <andrebemferraz@gmail.com>
2021-03-06 15:40:32 -0300

    Request IV

--- FUNDAMENTALS/BLOCO_4/DIA_3/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_3/scripts.js
@@ -1,6 +1,6 @@
 // Request I --------------------------------
 
-let n = 6;
+let n = 7;
 
 for (i = 0; i < n; i += 1) {
    console.log("*".repeat(n))
@@ -24,4 +24,20 @@ for (i = 1; i <= n; i += 1) {
   firstSpaces = firstSpaces.slice(0, -1)
   newSpaces = firstSpaces + "*".repeat(i)
   console.log(newSpaces)
+}
+
+// Request IV --------------------------------
+
+let middle = (n + 1) / 2;
+let left = " ".repeat(middle);
+let right = " ".repeat(middle);
+
+let newLeft = left
+let newRight = right
+
+for (i = 1; i <= n; i += 2) {
+  newLeft = newLeft.slice(0,-1);
+  newRight = newRight.slice(1);
+  let final = newLeft + "*".repeat(i) + newRight;
+  console.log(final)
 }
