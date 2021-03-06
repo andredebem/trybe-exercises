a4500f14a2e69a46faa3dfe68c02d563dd12a643
André De Bem <andrebemferraz@gmail.com>
2021-03-06 17:12:27 -0300

    Request I

--- FUNDAMENTALS/BLOCO_4/DIA_4/scripts.js
+++ FUNDAMENTALS/BLOCO_4/DIA_4/scripts.js
@@ -1,1 +1,11 @@
 // Let's start
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+// Request I ----------------------------------------
+
+console.log(`Bem vinda, ${info.personagem}`)
