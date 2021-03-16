8c27cf688c74dff409a25593054bc61a898360b9
André De Bem <andrebemferraz@gmail.com>
2021-03-16 10:28:46 -0300

    Request II

--- FUNDAMENTALS/BLOCO_5/DIA_1/script.js
+++ FUNDAMENTALS/BLOCO_5/DIA_1/script.js
@@ -12,4 +12,4 @@
         */
 
 document.getElementsByTagName("p")[1].innerText = "Em portugal, trabalhando e honrando a Nosso Senhor"
-document.getElementsByClassName("main-content")[0].style.backgroundColor = "blue"
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76, 164, 109)"
