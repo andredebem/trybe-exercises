dc2c7a9616751c983ec5e0faaa0e218368c0f922
André De Bem <andrebemferraz@gmail.com>
2021-04-20 19:13:09 -0300

    Part I - Request I

---
+++ FUNDAMENTALS/BLOCO_7/DIA_2/script.js
@@ -0,0 +1,53 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      margherita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (fullOrder) => {
+  // Adicione abaixo as informações necessárias.
+  const { name, phoneNumber, address, order } = fullOrder;
+  const arrayAddress = Object.values(address);
+  const arrayOrder = Object.values(order);
+  const deliveryName = arrayOrder[arrayOrder.length - 1];
+  return `Olá ${deliveryName.deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, ${arrayAddress[0]}, N: ${arrayAddress[1]}, AP: ${arrayAddress[2]}.`
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+}
+
+orderModifier(order);
