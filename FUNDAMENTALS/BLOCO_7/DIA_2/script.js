fd344d3f3b839e7fe1918dc7dab3395f315e2a98
André De Bem <andrebemferraz@gmail.com>
2021-04-21 14:58:17 -0300

    Part II - Request I

--- FUNDAMENTALS/BLOCO_7/DIA_2/script.js
+++ FUNDAMENTALS/BLOCO_7/DIA_2/script.js
@@ -55,3 +55,32 @@ const orderModifier = (fullOrder) => {
 }
 
 orderModifier(order);
+
+// ------------------------------------ Part II
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addTurnoManha = (les2, key, value) => {
+  les2[key] = value;
+  console.log(les2);
+}
+
+addTurnoManha(lesson2, 'turno', 'manhã');
